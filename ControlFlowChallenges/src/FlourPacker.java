7d3a2cc925d8d51ba642f87083311c7dfb1aa77b
John Stading <82844073+jstading@users.noreply.github.com>
2024-10-05 17:49:04 -0500

    Add files via upload

--- ControlFlowChallenges/src/FlourPacker.java
+++ ControlFlowChallenges/src/FlourPacker.java
@@ -9,45 +9,27 @@ public class FlourPacker {
     }
 
     public static boolean canPack(int bigCount, int smallCount, int goal){
-        int bigWeight = bigCount *5;
-        boolean packed = false;
+
+        int BIG_WEIGHT = 5;
 
         if ((bigCount < 0) || (smallCount < 0) || (goal < 0)){
             return false;
-        } else if (goal <= (bigWeight)){
-            int remainder = goal % bigWeight;
-            if (smallCount >= remainder){
-                packed = true;
-            }
-        } else if (bigWeight < goal){
-            int remainder = goal - bigWeight;
-            if (remainder <= smallCount){
-                packed = true;
-            }
         }
-        return packed;
-    }
 
-    /*public static boolean canPack(int bigCount, int smallCount, int goal){
-        boolean packed = false;
-        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)){
-            return false;
-        } else if ((goal <= ((bigCount*5) + (smallCount))) ){
-            for (int i = 0; i <=bigCount; i++){
-                if (i*5 + smallCount >= goal){
-                    packed = true;
-                } else if ((goal % 5 == 0)){
-                    packed = true;
-                }
-            }
-            if (bigCount * 5 > goal){
-                packed = false;
+        boolean result = false;
+        int totalBigweight = bigCount * BIG_WEIGHT;
+        if (goal <= (totalBigweight)){
+            int remaining = goal % BIG_WEIGHT;
+            if (smallCount >= remaining){
+                result = true;
             }
         } else {
-            packed = false;
+            if (smallCount >= goal - totalBigweight){
+                result = true;
+            }
         }
-    return packed;
-    }*/
+        return result;
+    }
 
 
 }
