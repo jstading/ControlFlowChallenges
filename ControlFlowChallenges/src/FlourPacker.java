2bcc3eafb996208923c3a5b9ccb67324a8f30b13
John Stading <82844073+jstading@users.noreply.github.com>
2024-10-05 06:10:02 -0500

    Add files via upload

--- ControlFlowChallenges/src/FlourPacker.java
+++ ControlFlowChallenges/src/FlourPacker.java
@@ -1,24 +1,53 @@
 // DEV
 public class FlourPacker {
     public static void main(String[] args){
-        System.out.println(canPack(4, 18, 19));
+        //System.out.println(canPack(4, 18, 19));
+        //System.out.println(canPack(2, 10, 18));
+        //System.out.println(canPack(0, 5, 4));
+        //System.out.println(canPack(1, 0, 4));
+        System.out.println(canPack(2, 1, 5));
     }
 
     public static boolean canPack(int bigCount, int smallCount, int goal){
+        int bigWeight = bigCount *5;
+        boolean packed = false;
+
         if ((bigCount < 0) || (smallCount < 0) || (goal < 0)){
             return false;
-        } else if ((goal <= ((bigCount*5) + (smallCount)))){
-            int bigFull = goal/5;
-            int remainder = goal%5;
-
-            if ((remainder <= smallCount) && (bigFull < bigCount)) {
-                return true;
-            } else {
-                return true;
+        } else if (goal <= (bigWeight)){
+            int remainder = goal % bigWeight;
+            if (smallCount >= remainder){
+                packed = true;
             }
-
+        } else if (bigWeight < goal){
+            int remainder = goal - bigWeight;
+            if (remainder <= smallCount){
+                packed = true;
+            }
         }
-        return true;
+        return packed;
     }
+
+    /*public static boolean canPack(int bigCount, int smallCount, int goal){
+        boolean packed = false;
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)){
+            return false;
+        } else if ((goal <= ((bigCount*5) + (smallCount))) ){
+            for (int i = 0; i <=bigCount; i++){
+                if (i*5 + smallCount >= goal){
+                    packed = true;
+                } else if ((goal % 5 == 0)){
+                    packed = true;
+                }
+            }
+            if (bigCount * 5 > goal){
+                packed = false;
+            }
+        } else {
+            packed = false;
+        }
+    return packed;
+    }*/
+
 
 }
